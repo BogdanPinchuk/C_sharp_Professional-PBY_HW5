9a0e4d98f69acbbe233740a6173b964691f25079
Bogdan <pinchuk.brus@gmail.com>
2019-09-30 13:13:41 +0300

    Add LesApp2

---
+++ LesApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LesApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            // join Unicode
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Інформація про файл
+            using (FileStream stream = new FileStream(@"Resources\TelephoneBook.xml", FileMode.Open))
+            {
+                // загрузка файла
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(stream);
+
+                // корневий елемент документа
+                XmlNode root = xmlDoc.DocumentElement;
+
+                Console.WriteLine("Номера телефонів:");
+
+                foreach (XmlNode elem in root.ChildNodes)
+                {
+                    Console.WriteLine($"\t{elem.InnerText}");
+                }
+            }
+
+            // delay
+            Console.ReadKey(true);
+        }
+    }
+}
