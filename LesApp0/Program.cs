f0fb97c1ebacdd6729c45954c4bd3261945629a3
Bogdan <pinchuk.brus@gmail.com>
2019-09-30 11:19:03 +0300

    Add LesApp0

---
+++ LesApp0/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LesApp0
+{
+    class Program
+    {
+        static void Main()
+        {
+            // join Unicode
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // створення .xml файла
+            using (XmlTextWriter xmlFile = new XmlTextWriter("TelephoneBook.xml", Encoding.Unicode))
+            {
+                // включаєм форматування
+                xmlFile.Formatting = Formatting.Indented;
+
+                // заголовок
+                xmlFile.WriteStartDocument();
+
+                // корнєвий елемент
+                {
+                    xmlFile.WriteStartElement("MyContacts");
+                    // тег
+                    {
+                        xmlFile.WriteStartElement("Contact");
+                        {
+                            // Атрибут
+                            xmlFile.WriteStartAttribute("TelephoneNumber");
+                            {
+                                // Ім'я
+                                xmlFile.WriteString("Bohdan");
+                            }
+                            xmlFile.WriteEndAttribute();
+                            xmlFile.WriteString("+380 (63) 12 34 567");
+                        }
+                        xmlFile.WriteEndElement();
+                    }
+                    // тег
+                    {
+                        xmlFile.WriteStartElement("Contact");
+                        {
+                            xmlFile.WriteStartAttribute("TelephoneNumber");
+                            {
+                                // Ім'я
+                                xmlFile.WriteString("Elena");
+                            }
+                            xmlFile.WriteEndAttribute();
+                            xmlFile.WriteString("+380 (64) 12 34 567");
+                        }
+                        xmlFile.WriteEndElement();
+                    }
+                    xmlFile.WriteEndElement();
+                }
+                xmlFile.WriteEndDocument();
+            }
+
+            // delay
+            Console.ReadKey(true);
+        }
+    }
+}
