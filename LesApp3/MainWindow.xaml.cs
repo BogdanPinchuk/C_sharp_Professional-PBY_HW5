018156efc5f7dfe6193379cf21a69e060e327548
Bogdan <pinchuk.brus@gmail.com>
2019-10-01 12:02:01 +0300

    Set properties for form elements

--- LesApp3/MainWindow.xaml.cs
+++ LesApp3/MainWindow.xaml.cs
@@ -23,7 +23,64 @@ namespace LesApp3
         public MainWindow()
         {
             InitializeComponent();
+
+            // стартовы налаштування
+
         }
 
+        private void SetFonts()
+        {
+            //lbText.FontFamily
+            
+        }
+
+        /// <summary>
+        /// При виборі кольору фону
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void СpGround_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            // установка кольору фону label
+            lbText.Background = new SolidColorBrush(cpGround.SelectedColor.Value);
+        }
+
+        /// <summary>
+        /// При виборі кольору тексту
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CpText_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            // установка кольору тексту label
+            lbText.Foreground = new SolidColorBrush(cpText.SelectedColor.Value);
+        }
+
+        /// <summary>
+        /// При зміні повзунка
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SSize_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // установка розміру тексту
+            lbText.FontSize = sSize.Value;
+            // вивдення розміру
+            if (tbSize != null)
+            {
+                tbSize.Text = lbText.FontSize.ToString("F0");
+            }
+        }
+
+        /// <summary>
+        /// При виборі стилю шрифта
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CbStyle_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // установка шрифта
+            //lbText.FontStyle
+        }
     }
 }
